438aa4bb42b7bd5ea1f1f07fd5b2be34edf8eccc
James Anwyl <ja.anwyl@gmail.com>
2020-07-23 13:06:00 +0100

    added gulp build pipeline, changed button color, refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,106 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+'use strict';
+/** Require dependencies. */
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-clean-css'),
+    htmlReplace = require('gulp-html-replace'),
+    maps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    del = require('del'),
+    connect = require('gulp-connect'),
+    open = require('gulp-open');
 
-exports.default = defaultTask
+/** Write .html files to the dist folder. */
+gulp.task('html', function() {
+    return gulp.src('./*.html') /** create stream from html files */
+        .pipe(htmlReplace({
+          'css': 'css/style.min.css',
+          'js': 'js/all.min.js'
+        }))
+        .pipe(gulp.dest('./dist')) /** write to dist folder */
+        .pipe(connect.reload()); /** use gulp-connect to reload the server */
+});
+
+/** Minify scripts. Create sourcemap and write to dist/js folder. */
+gulp.task('scripts', function() {
+    return gulp.src('js/**/*.js') 
+        .pipe(maps.init()) 
+        .pipe(concat('all.js')) 
+        .pipe(uglify()) 
+        .pipe(rename('all.min.js')) 
+        .pipe(maps.write('./')) 
+        .pipe(gulp.dest('./dist/js')) 
+        .pipe(connect.reload()); 
+});
+
+/** Compile + minify sass files. Create sourcemap and write to dist/css folder. */
+gulp.task('styles', function() {
+    return gulp.src([ 
+        'css/_colors.scss',
+        'css/style.scss'
+    ])
+        .pipe(maps.init())
+        .pipe(sass())
+        .pipe(rename('style.css'))
+        .pipe(minifyCss())
+        .pipe(rename('style.min.css'))
+        .pipe(maps.write('./')) 
+        .pipe(gulp.dest('./dist/css')) 
+        .pipe(connect.reload()); 
+});
+
+/** Optimise + copy images into dist/img */
+gulp.task('images', function() {
+    return gulp.src(['img/*']) 
+        .pipe(imagemin()) 
+        .pipe(gulp.dest('dist/img')); 
+});
+/** Copy favicons into dist folder */
+gulp.task('favicons', function(){
+  return gulp.src(['./*.ico', './*.png'])
+      .pipe(gulp.dest('dist/'));
+})
+
+/** Copy data folder into dist folder */
+gulp.task('data', function() {
+  return gulp.src(['data/*']) 
+      .pipe(gulp.dest('dist/data')); 
+});
+
+
+/** Create a web server on port 3000 using gulp-connect */
+gulp.task('connect', function() {
+    connect.server({
+        root: 'dist', 
+        port: 3000, 
+        livereload: true 
+    });
+});
+
+/** Open index.html in the browser with gulp-open. */
+gulp.task('open', function() {
+    gulp.src('dist/index.html')
+        .pipe(open({uri: 'http://localhost:3000/'}));  
+});
+
+/** Remove all files and folders inside dist folder, using del package */
+gulp.task('clean', function() {
+    return del('dist/**/*');
+});
+
+/** Watch index.html, and all files in sass and js folders for changes. */
+gulp.task('watch', function() {
+    gulp.watch('./*.html', ['html']); 
+    gulp.watch('css/**/*', ['styles']); 
+    gulp.watch('js/**/*', ['scripts']); 
+});
+
+/** Build task. Clean up dist folder, combine and minify sass/js, optimize images, write files to dist folder. */
+gulp.task('build', gulp.series('clean', 'html', 'scripts', 'styles', 'images', 'favicons', 'data'));
+
+/** Default task. Run build task first then; create server, open in browser, and watch sass, js and index.html files for changes. */
+gulp.task('default', gulp.series('build', 'connect', 'open', 'watch', function() {
+}));
